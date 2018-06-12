48cdcea79dd9c4921ddd8ebb64b8c2741019ee5f
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2018-06-11 21:34:45 -0300

    implement simple endpoints

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require("express")
 
 const app = express()
 
-app.get("/", (request, response, next) => {
-  response.status(200).send("Hello")
+app.get("/health", (request, response, next) => {
+  response.status(200)
+})
+
+app.get("/heavy", (request, response, next) => {
+  setTimeout(() => {
+    response.status(200).send("Heavy!!!\n")
+  }, 1000)
+})
+
+app.get("/light", (request, response, next) => {
+  setTimeout(() => {
+    response.status(200).send("Light!!!\n")
+  }, 200)
 })
 
 app.listen(8080)
