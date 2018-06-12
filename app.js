67d3e7cbe58812cc77e6ac297423d47359acb80f
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2018-06-12 13:32:59 -0300

    implement slow and fast endpoints

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ app.get("/health", (request, response, next) => {
 })
 
 app.get("/slow", (request, response, next) => {
-  setTimeout(() => {
-    response.status(200).send("Slow!!!\n")
-  }, 1000)
+  for (let i = 1; i < 5000000000; i++) {}
+  // response.status(200).send("Slow!!!")
+  response.status(200).send(`Slow!!! ${process.env.MY_SECRET}\n`)
 })
 
 app.get("/fast", (request, response, next) => {
-  setTimeout(() => {
-    response.status(200).send("Fast!!!\n")
-  }, 200)
+  for (let i = 1; i < 1000000000; i++) {}
+  // response.status(200).send("Fast!!!")
+  response.status(200).send(`Fast!!! ${process.env.MY_SECRET}\n`)
 })
 
 app.listen(8080)
