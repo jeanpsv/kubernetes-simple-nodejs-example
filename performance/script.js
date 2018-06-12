0d98552c70c65a1d014f96b3c71d701a3fa37202
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2018-06-12 13:33:59 -0300

    improve performance script

--- performance/script.js
+++ performance/script.js
@@ -1,19 +1,20 @@
 import { check } from "k6"
 import http from "k6/http"
 
-const host = "http://..."
+const host = "http://"
 
 export const options = {
   stages: [
+    { duration: "1m", target: 100 },
     { duration: "1m", target: 200 },
+    { duration: "1m", target: 300 },
     { duration: "1m", target: 400 },
+    { duration: "1m", target: 500 },
+    { duration: "1m", target: 600 },
+    { duration: "1m", target: 700 },
     { duration: "1m", target: 800 },
+    { duration: "1m", target: 900 },
     { duration: "1m", target: 1000 },
-    { duration: "1m", target: 1200 },
-    { duration: "1m", target: 1400 },
-    { duration: "1m", target: 1600 },
-    { duration: "1m", target: 1800 },
-    { duration: "1m", target: 2000 },
   ]
 }
 
