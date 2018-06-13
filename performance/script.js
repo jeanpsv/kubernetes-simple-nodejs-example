6727e9b2d4dacb70a0ba0fc84c6163ce8fa454d6
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2018-06-13 09:23:52 -0300

    configure script to receive external var

--- performance/script.js
+++ performance/script.js
@@ -1,32 +1,19 @@
 import { check } from "k6"
 import http from "k6/http"
 
-const host = "http://"
-
 export const options = {
   stages: [
-    { duration: "1m", target: 100 },
     { duration: "1m", target: 200 },
-    { duration: "1m", target: 300 },
     { duration: "1m", target: 400 },
-    { duration: "1m", target: 500 },
     { duration: "1m", target: 600 },
-    { duration: "1m", target: 700 },
     { duration: "1m", target: 800 },
-    { duration: "1m", target: 900 },
     { duration: "1m", target: 1000 },
   ]
 }
 
 export default function () {
   check(
-    http.get(`${host}/fast`), {
-      "http 200": (res) => res.status === 200
-    }
-  )
-
-  check(
-    http.get(`${host}/slow`), {
+    http.get(`${process.env.SERVICE_HOST}/fast`), {
       "http 200": (res) => res.status === 200
     }
   )
